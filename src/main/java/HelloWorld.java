de7ab73ee3fe0a1764f1af3e5ebb7110079d270b
yinyg <yinyougen@aliyun.com>
2022-05-12 09:34:48 +0800

    update

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -11,22 +11,6 @@ import java.util.stream.Collectors;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2 ,3, 4);
-        list = list.stream()
-                .sorted(new Comparator<Integer>() {
-                    @Override
-                    public int compare(Integer o1, Integer o2) {
-                        if (o1 < 02) {
-                            return 1;
-                        } else if (o1 > o2) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    }
-                })
-                .collect(Collectors.toList());
-        System.out.println(1);
     }
 
 }
