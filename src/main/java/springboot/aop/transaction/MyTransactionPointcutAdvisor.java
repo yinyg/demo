e4e3311f896b673534319bb7700948397a23c0e3
yinyg <yinyougen@aliyun.com>
2023-05-06 17:04:53 +0800

    自定义事务注解

--- src/main/java/springboot/aop/transaction/MyTransactionPointcutAdvisor.java
+++ src/main/java/springboot/aop/transaction/MyTransactionPointcutAdvisor.java
@@ -3,9 +3,12 @@ package springboot.aop.transaction;
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractBeanFactoryPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Role;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 
 /**
  * @author yinyg
@@ -17,7 +20,39 @@ public class MyTransactionPointcutAdvisor extends AbstractBeanFactoryPointcutAdv
 
     private final MyTransactionPointcut myTransactionPointcut = new MyTransactionPointcut();
 
-    private final MyTransactionPointcutInterceptor myTransactionPointcutInterceptor = new MyTransactionPointcutInterceptor();
+    private final MyTransactionPointcutInterceptor myTransactionPointcutInterceptor = new MyTransactionPointcutInterceptor() {
+        /**
+         * getPlatformTransactionManager
+         *
+         * @return org.springframework.transaction.PlatformTransactionManager
+         * @throws
+         * @author yinyg
+         * @date 2023/5/6
+         */
+        @Override
+        public PlatformTransactionManager getPlatformTransactionManager() {
+            return platformTransactionManager;
+        }
+
+        /**
+         * getTransactionDefinition
+         *
+         * @return org.springframework.transaction.TransactionDefinition
+         * @throws
+         * @author yinyg
+         * @date 2023/5/6
+         */
+        @Override
+        public TransactionDefinition getTransactionDefinition() {
+            return transactionDefinition;
+        }
+    };
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Autowired
+    private TransactionDefinition transactionDefinition;
 
     /**
      * getPointcut
--- src/main/java/springboot/aop/transaction/MyTransactionPointcutInterceptor.java
+++ src/main/java/springboot/aop/transaction/MyTransactionPointcutInterceptor.java
@@ -1,16 +1,19 @@
 package springboot.aop.transaction;
 
+import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.transaction.interceptor.TransactionInterceptor;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 
 /**
  * 自定义事务注解Interceptor
  * @author yinyg
  * @date 2023/5/5
  */
-public class MyTransactionPointcutInterceptor extends TransactionInterceptor {
+public class MyTransactionPointcutInterceptor implements MethodInterceptor {
 
     public final Log log = LogFactory.getLog(MyTransactionPointcutInterceptor.class);
 
@@ -23,10 +26,50 @@ public class MyTransactionPointcutInterceptor extends TransactionInterceptor {
      * @author yinyg
      * @date 2023/5/5
      */
-    @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         log.info("invoke method " + invocation.getMethod().getDeclaringClass().getName() + "#" + invocation.getMethod().getName() + " with MyTransactionPointcutInterceptor");
-        return super.invoke(invocation);
+        PlatformTransactionManager platformTransactionManager = getPlatformTransactionManager();
+        TransactionDefinition transactionDefinition = getTransactionDefinition();
+        // 开启事务
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(transactionDefinition);
+        Object result = null;
+        try {
+            result = invocation.proceed();
+            // 提交事务
+            platformTransactionManager.commit(transactionStatus);
+            log.info("提交事务");
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            // 回滚事务
+            platformTransactionManager.rollback(transactionStatus);
+            log.error("回滚事务");
+            throw e;
+        }
+        return result;
+    }
+
+    /**
+     * getPlatformTransactionManager
+     *
+     * @return org.springframework.transaction.PlatformTransactionManager
+     * @throws
+     * @author yinyg
+     * @date 2023/5/6
+     */
+    public PlatformTransactionManager getPlatformTransactionManager() {
+        return null;
+    }
+
+    /**
+     * getTransactionDefinition
+     *
+     * @return org.springframework.transaction.TransactionDefinition
+     * @throws
+     * @author yinyg
+     * @date 2023/5/6
+     */
+    public TransactionDefinition getTransactionDefinition() {
+        return null;
     }
 
 }
