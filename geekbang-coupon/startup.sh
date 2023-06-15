7d769e8c0e00f5f9029d1747142d9d5ba22fa541
yinyg <yinyougen@aliyun.com>
2023-06-15 18:27:32 +0800

    调整nacos demo docker-compose部署编排脚本

--- geekbang-coupon/startup.sh
+++ geekbang-coupon/startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# 先启动nacos，再构建启动应用，防止应用启动时，nacos还没有完成启动
+# 先启动nacos，等待60秒，然后再构建启动应用，防止应用启动时，nacos还没有完成启动
 docker-compose -f docker-compose-nacos.yml up -d
+
+sleep 60s
 
 mvn clean package -Dmaven.test.skip=true
 
